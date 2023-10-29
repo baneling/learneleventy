770effedeec3192c463ef8cfc49fdb215974b7a3
baneling <hdapperloo@gmail.com>
2023-10-30 00:27:18 +0100

    tailwind css added

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,24 @@
+const { DateTime  } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     // addPassthroughCopy points at dirs/files to be copied to _site
-    eleventyConfig.addPassthroughCopy("src/assets/css")
-    eleventyConfig.addPassthroughCopy("src/assets/images")
+    eleventyConfig.addPassthroughCopy("src/assets/css");
+    eleventyConfig.addPassthroughCopy("src/assets/images");
+    eleventyConfig.addPassthroughCopy({"src/robots.txt": "robots.txt"});
 
     // Order the pages on the navbar by order data
     eleventyConfig.addCollection("page", function(collections) {
         return collections.getFilteredByTag("page").sort(function(a, b) {
             return a.data.order - b.data.order;
         });
+    });
+
+    // DateTime
+    eleventyConfig.addShortcode("currentDate", (date = DateTime.now()) => {
+        return date;
+    });
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
 
     // Shortcode tutorial
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{md,liquid}", "./src/**/*.svg"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
